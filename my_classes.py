39bd5ba1c5cbff471a737e45afb48f1c0213caf7
mak2494 <km2430@mci4me.at>
2025-04-07 13:44:35 +0200

    generelles Aufgaben repository

---
+++ my_classes.py
@@ -0,0 +1,21 @@
+class Subject():
+    def __init__(self, first_name, last, sex):
+        pass
+
+    def estimate_max_hr(self):
+        """A function that estimates the maximum heart rate of a subject"""
+        pass
+
+class Supervisor():
+    def __init__(self, first_name, last_name):
+        pass
+
+class Experiment():
+    def __init__(self, name, date):
+        pass
+
+def add_subject(self, subject):
+    self.subject = subject
+
+def add_supervisor(self, supervisor):
+    pass
